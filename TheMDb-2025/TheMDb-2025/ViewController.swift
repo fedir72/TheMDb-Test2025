f41ee919b1427f14a46d76664fc659c7c8a2a8cb
Ihor Fedii <fiodor312154@gmail.com>
2025-08-01 21:41:17 +0200

    Empty project

---
+++ TheMDb-2025/TheMDb-2025/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMDb-2025
+//
+//  Created by ihor fedii on 01.08.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
